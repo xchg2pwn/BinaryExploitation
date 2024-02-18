15a60f7688e6a167d434327d12a5d95a435acaa9
xchg2pwn <xchg2pwn@gmail.com>
2024-02-18 17:48:14 -0600

    add

---
+++ HackTheBox/Retired/activate_license.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_FILES['licensefile'])) {
+    $license      = file_get_contents($_FILES['licensefile']['tmp_name']);
+    $license_size = $_FILES['licensefile']['size'];
+
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if (!$socket) { echo "error socket_create()\n"; }
+
+    if (!socket_connect($socket, '127.0.0.1', 1337)) {
+        echo "error socket_connect()" . socket_strerror(socket_last_error()) . "\n";
+    }
+
+    socket_write($socket, pack("N", $license_size));
+    socket_write($socket, $license);
+
+    socket_shutdown($socket);
+    socket_close($socket);
+}
+?>
